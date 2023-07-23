6cab44b066ba3308318f6c1212b2b672ee298deb
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-07-23 23:42:14 +0600

    cart collection apis updated

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
         const menuCollection = client.db("bistroDb").collection("menu");
         const reviewCollection = client.db("bistroDb").collection("reviews");
+        const cartCollection = client.db("bistroDb").collection("carts");
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -40,6 +41,24 @@ async function run() {
             res.send(result);
         })
 
+
+        // cart collection apis
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
